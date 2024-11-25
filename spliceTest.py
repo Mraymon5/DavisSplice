343e1a18f0c0b666c34839378b82d1c5f61c4e15
Raymond <63129961+Mraymon5@users.noreply.github.com>
2024-11-25 18:26:05 -0500

    Update spliceTest.py

--- spliceTest.py
+++ spliceTest.py
@@ -12,12 +12,15 @@ GPIO.cleanup()
 GPIO.setmode(GPIO.BOARD)
 
 #define the pin that goes to the beam break for shutter open/close detection
-Shutter_inport = 5
+Shutter_inport = 5 # Should be wired to Shutter Mag Sensor channel
 GPIO.setup(Shutter_inport, GPIO.IN)
 
 #define the pin that goes to the lick detector (the photodiode)
-Lick_inport = 7
+Lick_inport = 7 # Should be wired to Lick TTL channel
 
+
+print(f'Shutter Channel is: {GPIO.input(Shutter_inport)}')
+print(f'Lick Channel is: {GPIO.input(Lick_inport)}')
 
     while shutteropen == 0:
         if GPIO.input(Shutter_inport) == GPIO.LOW:     #if IR beam is broken

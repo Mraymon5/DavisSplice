ad8a344bc4782c591204db4c72fc13d28144d618
Raymond <63129961+Mraymon5@users.noreply.github.com>
2024-11-25 18:18:11 -0500

    Update spliceTest.py

--- spliceTest.py
+++ spliceTest.py
@@ -1,1 +1,19 @@
 #Setting up a file to test the pi inputs from the splicer
+import RPi.GPIO as GPIO
+import time
+import numpy as np
+import easygui
+#import sys
+
+#%% Setup
+# Setup pi board
+GPIO.setwarnings(False)
+GPIO.cleanup()
+GPIO.setmode(GPIO.BOARD)
+
+#define the pin that goes to the beam break for shutter open/close detection
+Shutter_inport = 5
+GPIO.setup(Shutter_inport, GPIO.IN)
+
+#define the pin that goes to the lick detector (the photodiode)
+Lick_inport = 7

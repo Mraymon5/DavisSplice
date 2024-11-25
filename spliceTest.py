8431707d1d25e1b4ed119c087ca453cb86730e3c
Raymond <63129961+Mraymon5@users.noreply.github.com>
2024-11-25 18:19:41 -0500

    Update spliceTest.py

--- spliceTest.py
+++ spliceTest.py
@@ -17,3 +17,8 @@ GPIO.setup(Shutter_inport, GPIO.IN)
 
 #define the pin that goes to the lick detector (the photodiode)
 Lick_inport = 7
+
+
+    while shutteropen == 0:
+        if GPIO.input(Shutter_inport) == GPIO.LOW:     #if IR beam is broken
+            shutteropen = 1                     #break out of while loop
